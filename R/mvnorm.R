fb222484a9e4a2c9ead375f0e166859ca0c7ab47
Sam Gardiner <sam.gardiner@gmail.com>
2018-08-24 15:45:11 +1000

    Added README

--- R/mvnorm.R
+++ R/mvnorm.R
@@ -4,7 +4,6 @@
 #' Simulate from a multivariate normal distribution.
 rmvnorm <- function(n, mu, sigma){
   p <- length(mu)
-
   E <- eigen(sigma)
   A <- E$vectors %*% sqrt(diag(E$values))
 
---
+++ README.md
@@ -0,0 +1,3 @@
+# sgthings
+
+This is a package of Sam Gardiner's personally useful R functions.
