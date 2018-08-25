b52ce720ec6ffe7e9fa11eb37a79c106ec42db4e
Sam Gardiner <sam.gardiner@gmail.com>
2018-08-25 17:58:06 +1000

    Adde multivariate normal density function.

--- R/mvnorm.R
+++ R/mvnorm.R
@@ -1,11 +1,30 @@
 # Functions for working with multivariate normal distributions.
 
-#' rmvnorm
-#' Simulate from a multivariate normal distribution.
+#' Generate random vectors from a multivariate normal distribution.
+#'
+#' @param n Number of random vectors to return.
+#' @param mu The mean vector.
+#' @param sigma The variance-covariance matrix.
+#' @return A matrix of \code{n} rows, each drawn from the MVN distribution with parameters \code{mu} and \code{sigma}.
+#'
 rmvnorm <- function(n, mu, sigma){
+  # @TODO: dimension checking.
   p <- length(mu)
   E <- eigen(sigma)
-  A <- E$vectors %*% sqrt(diag(E$values))
+  A <- E$vectors %*% diag(sqrt(E$values))
 
-  matrix(rnorm(n * p), ncol = p) %*% A + mu
+  t(A %*% matrix(rnorm(n * p), ncol = n) + mu)
+}
+
+#' The multivariate normal density function.
+#'
+#' @param x A p-dimension vector.
+#' @param mu A p-dimensional mean vector.
+#' @param sigma A p \times p variance-covariance matrix.
+#'
+dmvnorm <- function(x, mu, sigma){
+  # @TODO: dimension checking.
+  p <- length(x)
+  c <- 1 / (2 * pi)^(1/p) * sqrt(det(sigma))
+  c * exp((-t(x - mu) %*% solve(sigma) %*% (x - mu)) / 2)
 }
